c42dc5cd9f49e2249239942775e10af68774d3f9
LuisAngelGabriel <luis_gabriel1@ucne.edu.do>
2025-05-25 20:31:49 -0400

    Realizando las migraciones de lugar

---
+++ RegistroTecnicoss/Migrations/20250525234221_AgregarCliente.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RegistroTecnicoss.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregarCliente : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cliente",
+                columns: table => new
+                {
+                    ClienteId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FechaIngreso = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Nombres = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Direccion = table.Column<string>(type: "TEXT", maxLength: 150, nullable: false),
+                    Rnc = table.Column<string>(type: "TEXT", maxLength: 11, nullable: false),
+                    LimiteCredito = table.Column<decimal>(type: "TEXT", nullable: false),
+                    TecnicoId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cliente", x => x.ClienteId);
+                    table.ForeignKey(
+                        name: "FK_Cliente_Tecnico_TecnicoId",
+                        column: x => x.TecnicoId,
+                        principalTable: "Tecnico",
+                        principalColumn: "TecnicoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cliente_TecnicoId",
+                table: "Cliente",
+                column: "TecnicoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cliente");
+        }
+    }
+}
