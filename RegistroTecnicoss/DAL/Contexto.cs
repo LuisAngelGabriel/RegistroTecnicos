e16f98ba0567d4e6b2e7af439217c03ff94caa99
LuisAngelGabriel <luis_gabriel1@ucne.edu.do>
2025-06-01 22:15:19 -0400

    Agregando "Tickets" al contexto.

--- RegistroTecnicoss/DAL/Contexto.cs
+++ RegistroTecnicoss/DAL/Contexto.cs
@@ -9,6 +9,7 @@ namespace RegistroTecnicoss.DAL
         public Contexto(DbContextOptions<Contexto> options) : base (options) { }
         public DbSet<Tecnicos> Tecnico { get; set; }
         public DbSet<Clientes> Cliente { get; set; }
+        public DbSet<Tickets> Ticket { get; set; }
 
     }
 }
